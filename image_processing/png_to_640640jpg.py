d4a20e7a1b0ed357c49014ff7df4be2f5b412f51
jwjungwoo <140131247+jwjungwoo@users.noreply.github.com>
2024-09-29 15:05:09 +0900

    Update png_to_640640jpg.py

--- image_processing/png_to_640640jpg.py
+++ image_processing/png_to_640640jpg.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 
-# 경로 설정
 folder_path = "C:\\Users\\HPUser\\Desktop\\data_set\\car"
 output_path = "C:\\Users\\HPUser\\Desktop\\data_set\\car_output"
 
@@ -10,11 +9,11 @@ files = os.listdir(folder_path)
 
 # 파일 이름을 001부터 시작하도록 설정
 file_index = 1
+total_files = len(files)
 
 for file in sorted(files):
     file_path = os.path.join(folder_path, file)
-    
-    # 이미지 파일만 처리하기 위해 확장자 확인
+
     if file.lower().endswith(('.png', '.jpg', '.jpeg')):
 
         image = cv2.imread(file_path)
@@ -22,20 +21,24 @@ for file in sorted(files):
             print(f"Error loading image: {file}")
             continue
         
-        if file.lower().endswith('.png'):
+        # 진행 상황 출력 (파일 인덱스와 전체 파일 수를 이용하여 퍼센트 계산)
+        print(f"Processing {file_index}/{total_files} ({(file_index/total_files)*100:.2f}%): {file}")
 
+        if file.lower().endswith('.png'):
             image = cv2.resize(image, (640, 640))
             new_filename = f"{file_index:03}.jpg"  # 001, 002, 003 등
             new_file_path = os.path.join(output_path, new_filename)
+
             cv2.imwrite(new_file_path, image)
-            
+
             file_index += 1
         else:
             image = cv2.resize(image, (640, 640))
             new_filename = f"{file_index:03}.jpg"
             new_file_path = os.path.join(output_path, new_filename)
+
             cv2.imwrite(new_file_path, image)
-            
+
             file_index += 1
 
-print("작업 완료")
+print("모든 작업 완료")
